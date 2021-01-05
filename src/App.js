272aec564af8acbbc8ea43f7ee51ef013edfff67
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2021-01-05 13:45:42 -0700

    removed cdn for bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Navbar, NavDropdown, Nav, Form, FormControl, Button, Card } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
