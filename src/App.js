75265998256696050d603b4076b9e558f243836b
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2021-01-05 10:36:29 -0700

    Added Boostrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand href='#home'>Covid-19 Vaccination Tracker</Navbar.Brand>
+      </Navbar>
     </div>
   );
 }
