5f933ba2d048a47a198ffdc17af0c8773a37df40
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2021-01-05 13:05:33 -0700

    added routing and pages

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button, Card } from 'react-bootstrap';
+export class About extends Component {
+  render() {
+    return (
+      <div className='container-fluid p-4'>
+        <Card>
+          <Card.Header as='h5'>About</Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>With supporting text below as a natural lead-in to additional content.</Card.Text>
+            <Button variant='primary'>Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import './App.css';
-import { Navbar, NavDropdown, Nav, Form, FormControl, Button } from 'react-bootstrap';
-
+import { Navbar, NavDropdown, Nav, Form, FormControl, Button, Card } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Sources from './Sources';
 function App() {
   return (
-    <div className='App'>
-      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-        <Navbar.Brand href='#home'>COVID-19 Vaccine Tracker Canada</Navbar.Brand>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='#features'>About</Nav.Link>
-            <Nav.Link href='#pricing'>Sources</Nav.Link>
-          </Nav>
-          {/* <Nav>
-            <NavDropdown title='Menu' id='collasible-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav> */}
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <Router>
+      <div>
+        <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+          <Navbar.Brand href='/'>COVID-19 Vaccine Tracker Canada</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='mr-auto'>
+              <Nav.Link href='/about'>About</Nav.Link>
+              <Nav.Link href='/sources'>Sources</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/sources'>
+            <Sources />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+// function Sources() {
+//   return <h2>Sources</h2>;
+// }
 
 export default App;
