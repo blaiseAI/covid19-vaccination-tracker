5812c04274a7fc9bc3ab79fab8b63afcca638896
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2021-01-05 12:28:42 -0700

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, NavDropdown, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className='App'>
-      <Navbar bg='dark' variant='dark'>
-        <Navbar.Brand href='#home'>Covid-19 Vaccination Tracker</Navbar.Brand>
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Navbar.Brand href='#home'>COVID-19 Vaccine Tracker Canada</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='#features'>About</Nav.Link>
+            <Nav.Link href='#pricing'>Sources</Nav.Link>
+          </Nav>
+          {/* <Nav>
+            <NavDropdown title='Menu' id='collasible-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav> */}
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
